71b18d383c0a60102c511cb77e626c3c2d30d03c
Beck Anderson <bianderson@madisoncollege.edu>
2021-04-27 15:18:39 -0500

    Client Side routing working
    need to visit the Network tab and see an adr bar change but no network call

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddStudentComponent } from './add-student/add-student.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: ""
+    , component: HomeComponent
+  }
+  ,   {
+    path: "add-student"
+    , component: AddStudentComponent
+  }
+  ,   {
+    path: "**"
+    , component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
